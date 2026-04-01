6c989e15910b5e17b0af49f8ddab3cfbcf6b261d
JinumolJames <jinumoljames847@gmail.com>
2026-04-01 22:08:04 +0530

    updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://coreshift-sqlite.onrender.com';
+const API_URL = 'https://coreshift-sqlite.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_URL,
